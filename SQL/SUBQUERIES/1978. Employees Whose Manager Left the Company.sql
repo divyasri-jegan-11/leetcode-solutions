0477c5e03833fa84c89517e995cd3aa650132e42
Divyasri Jegan <155663035+divyasri-jegan-11@users.noreply.github.com>
2025-05-06 21:42:54 +0530

    #1978

--- SQL/SUBQUERIES/1978. Employees Whose Manager Left the Company.sql
+++ SQL/SUBQUERIES/1978. Employees Whose Manager Left the Company.sql
@@ -1,1 +1,8 @@
 
+SELECT e.employee_id FROM Employees AS e 
+LEFT JOIN Employees AS Manager 
+ON (e.manager_id = Manager.employee_id)
+WHERE e.salary< 30000
+ AND e.manager_id IS NOT NULL
+ AND Manager.employee_id IS NULL
+ ORDER BY 1;
